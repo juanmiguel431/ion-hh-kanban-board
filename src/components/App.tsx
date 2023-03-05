5662b3aea3f8a1865c39e0de9b94e3cb8f9772f1
juanmiguel431 <juanmiguel431@gmail.com>
2023-03-05 16:47:09 -0400

    Delete confirmation added.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,52 @@
 import React, { useState } from 'react';
 import Board from './Board';
-import { Button, Form, Modal } from 'antd';
-import { PlusOutlined } from '@ant-design/icons';
+import { Affix, Button, Divider, Form, Modal } from 'antd';
+import { ExclamationCircleFilled, PlusOutlined } from '@ant-design/icons';
 import CardForm from './CardForm';
 import { ICard } from '../models';
+
+const { confirm } = Modal;
+
+const data: ReactTrello.BoardData = {
+  lanes: [
+    {
+      id: 'lane1',
+      title: 'To do',
+      label: '2/2',
+      cards: [
+        { id: 'Card1', title: 'Write Blog', description: 'Can AI make memes', label: '30 mins', draggable: false },
+        {
+          id: 'Card2',
+          title: 'Pay Rent',
+          description: 'Transfer via NEFT',
+          label: '5 mins',
+          metadata: { sha: 'be312a1' }
+        }
+      ]
+    },
+    {
+      id: 'lane2',
+      title: 'In progress',
+      label: '0/0',
+      cards: []
+    },
+    {
+      id: 'lane3',
+      title: 'Done',
+      label: '2/2',
+      cards: [
+        { id: 'Card1', title: 'Write Blog', description: 'Can AI make memes', label: '30 mins', draggable: false },
+        {
+          id: 'Card2',
+          title: 'Pay Rent',
+          description: 'Transfer via NEFT',
+          label: '5 mins',
+          metadata: { sha: 'be312a1' }
+        }
+      ]
+    },
+  ]
+}
 
 export const App: React.FC = () => {
   const [modalOpened, setModalOpened] = useState<boolean>(false);
@@ -11,16 +54,31 @@ export const App: React.FC = () => {
 
   return (
     <div className="app">
-      <Button
-        type="primary"
-        icon={<PlusOutlined/>}
-        onClick={_ => {
-          form.resetFields();
-          setModalOpened(true);
+      <Affix offsetTop={10}>
+        <Button
+          type="primary"
+          icon={<PlusOutlined/>}
+          onClick={_ => {
+            form.resetFields();
+            setModalOpened(true);
+          }}
+        >Add new</Button>
+      </Affix>
+
+      <Divider/>
+
+      <Board
+        data={{ ...data }}
+        onBeforeCardDelete={cb => {
+          confirm({
+            title: 'Do you want to delete this item?',
+            icon: <ExclamationCircleFilled/>,
+            onOk: async () => {
+              cb();
+            }
+          });
         }}
-      >Add new</Button>
-      <br/><br/>
-      <Board/>
+      />
 
       <Modal
         maskClosable={false}
@@ -36,7 +94,7 @@ export const App: React.FC = () => {
           })
         }}
       >
-        <CardForm form={form} />
+        <CardForm form={form}/>
       </Modal>
     </div>
   );
--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,40 +1,21 @@
 import React from 'react';
 import { default as TrelloBoard } from 'react-trello';
 
-export const Board: React.FC = () => {
+interface IProps {
+  data: ReactTrello.BoardData;
+  onCardDelete?: (cardId: string, laneId: string) => void;
+  onBeforeCardDelete?: (callback: Function) => void;
+}
 
-  const data = {
-    lanes: [
-      {
-        id: 'lane1',
-        title: 'To do',
-        label: '2/2',
-        cards: [
-          {id: 'Card1', title: 'Write Blog', description: 'Can AI make memes', label: '30 mins', draggable: false},
-          {id: 'Card2', title: 'Pay Rent', description: 'Transfer via NEFT', label: '5 mins', metadata: {sha: 'be312a1'}}
-        ]
-      },
-      {
-        id: 'lane2',
-        title: 'In progress',
-        label: '0/0',
-        cards: []
-      },
-      {
-        id: 'lane3',
-        title: 'Done',
-        label: '2/2',
-        cards: [
-          {id: 'Card1', title: 'Write Blog', description: 'Can AI make memes', label: '30 mins', draggable: false},
-          {id: 'Card2', title: 'Pay Rent', description: 'Transfer via NEFT', label: '5 mins', metadata: {sha: 'be312a1'}}
-        ]
-      },
-    ]
-  }
+export const Board: React.FC<IProps> = ({ data, onCardDelete, onBeforeCardDelete }) => {
 
   return (
     <div className="board">
-      <TrelloBoard data={data} />
+      <TrelloBoard
+        data={data}
+        onCardDelete={onCardDelete}
+        onBeforeCardDelete={onBeforeCardDelete}
+      />
     </div>
   );
 }
