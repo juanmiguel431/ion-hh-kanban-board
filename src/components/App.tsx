d596d3b5d74844c14cabdc2cb7d5f519348ab6d5
juanmiguel431 <juanmiguel431@gmail.com>
2023-03-05 15:13:42 -0400

    Step 1 - Add new Todo button and form added.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Board from './Board';
+import { Button, Form, Modal } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
+import CardForm from './CardForm';
 
 export const App: React.FC = () => {
+  const [modalOpened, setModalOpened] = useState<boolean>(false);
+  const [form] = Form.useForm();
+
   return (
     <div className="app">
-      My App
-      <Board />
+      <Button
+        type="primary"
+        icon={<PlusOutlined/>}
+        onClick={_ => {
+          form.resetFields();
+          setModalOpened(true);
+        }}
+      >Add new</Button>
+      <br/><br/>
+      <Board/>
+
+      <Modal
+        maskClosable={false}
+        open={modalOpened}
+        title="New To do"
+        onCancel={_ => {
+          setModalOpened(false);
+        }}
+        onOk={_ => {
+          form.validateFields().then(values => {
+            console.log(values);
+            setModalOpened(false);
+          })
+        }}
+      >
+        <CardForm form={form} />
+      </Modal>
     </div>
   );
 }
